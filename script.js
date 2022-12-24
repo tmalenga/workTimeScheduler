e22a8da313a8c73ab70877d048a0dbca49772c17
tmalenga0@gmail.com <tmalenga0@gmail.com>
2022-12-23 22:08:47 -0400

    Add function to check the id of the timeslot

--- script.js
+++ script.js
@@ -30,10 +30,14 @@ $(function () {
       }) 
 
     // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+        function checkTense(){
+            console.log(timeSlots)     
+            // timeSlots[0].className = "row future"
+            console.log(timeSlots[0].className)
+            let previousData = JSON.parse(localStorage.getItem("User_Data"))||{}
+            console.log("!!!!!!!!!!!!!!", Object.entries(previousData));
+            let arrayPreviousData = Object.entries(previousData);
+        }
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
@@ -46,6 +50,8 @@ $(function () {
       }
     
       toDisplayTime();
+      setInterval(toDisplayTime, 1000);
+      checkTense();
 
 
   });

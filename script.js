54151fb1a0916b1ccb83d2aa1f863efa74df1325
tmalenga0@gmail.com <tmalenga0@gmail.com>
2022-12-23 23:31:19 -0400

    removing comments and cleanup (final)

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 // Define variables
-//let user_data = $("textarea");
-//let sibs = $("btn-9").siblings;
-//let timeRange = $("div .col-2");
 let displayTime = $("#currentDay");
 let currentHour = dayjs().hour();
 const timeSlots = document.querySelectorAll(".container-fluid > div");
-console.log(timeSlots[0].id)
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 $(function () {
     // TODO: Add a listener for click events on the save button.
     $("button").click(function(){
-        console.log($(this).parent().attr("id"));
+        // Use DOM traversal to get access elements and attributes in HTML
         let userInput = $(this).siblings("textarea").val();
-        //let timeRange = $(this).siblings("div .col-2").html();
         let timeRange = $(this).parent().attr("id");
-        console.log(timeRange + "IT WORKS!! YAYAYA! - maybe but still YAYAYA");
         
         //accessing local storage to update for any input saved by the user 
         let userData = JSON.parse(localStorage.getItem("User_Data"))||{}  
-        userData[timeRange] = userInput;
-          // time: timeRange, 
-          // taskItem: userInput       
+        userData[timeRange] = userInput;     
         
-        console.log(userData);  
         let userData_serialized = JSON.stringify(userData);
         localStorage.setItem("User_Data", userData_serialized);         
   
@@ -33,7 +24,6 @@ $(function () {
         function checkTense(){                
             
             let previousData = JSON.parse(localStorage.getItem("User_Data"))||{}
-            console.log("!!!!!!!!!!!!!!", Object.entries(previousData));
             let arrayPreviousData = Object.entries(previousData);            
             
             for (let i = 0; i < arrayPreviousData.length; i++){                

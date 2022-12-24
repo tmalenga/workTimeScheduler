46e725f549d290aeab82d3d4fca01f943aefa090
tmalenga0@gmail.com <tmalenga0@gmail.com>
2022-12-23 23:10:36 -0400

    Adding classes to name past/ present or future blocks

--- script.js
+++ script.js
@@ -29,41 +29,51 @@ $(function () {
   
       }) 
 
-    // TODO: Add code to apply the past, present, or future class to each time
+    // TODO: Add code to apply the past, present, or future class to each time  & add code to get any user input that was saved in localStorage and set
         function checkTense(){
             console.log(timeSlots)     
             // timeSlots[0].className = "row future"
             console.log(timeSlots[0].className)
             let previousData = JSON.parse(localStorage.getItem("User_Data"))||{}
             console.log("!!!!!!!!!!!!!!", Object.entries(previousData));
-            let arrayPreviousData = Object.entries(previousData);
+            let arrayPreviousData = Object.entries(previousData);            
             
-        for (let i = 0; i < arrayPreviousData.length; i++){
-            console.log("---------", $(`#${arrayPreviousData[i][0]}`))
-            $(`#${arrayPreviousData[i][0]}`).children("textarea").val(arrayPreviousData[i][1]);
-        }
+            for (let i = 0; i < arrayPreviousData.length; i++){
+                console.log("---------", $(`#${arrayPreviousData[i][0]}`))
+                $(`#${arrayPreviousData[i][0]}`).children("textarea").val(arrayPreviousData[i][1]);
+            }
 
-        for (var i = 0; i < timeSlots.length;i++){
-            let myId = timeSlots[i].id;
-            let myhour = myId.slice(5);
-            console.log(myhour);
-        }
+            for (var i = 0; i < timeSlots.length;i++){
+                let myId = timeSlots[i].id;
+                let myhour = myId.slice(5);
+                console.log(myhour);
+                let newhour = parseInt(myhour);        
+                console.log(newhour);
+                console.log(timeSlots[i].className)
+                if (currentHour > newhour){
+                    timeSlots[i].className = "row time-block past"
+                    console.log(timeSlots[i].className)
+                }
+                else if(currentHour === newhour){
+                    timeSlots[i].className = "row time-block present"
+                    console.log(timeSlots[i].className)
+                }
+                else{
+                    timeSlots[i].className = "row time-block future"
+                    console.log(timeSlots[i].className)
+                }
+            }
 
-        }
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
+        }    
+
     // TODO: Add code to display the current date in the header of the page.
     function toDisplayTime(){
         let currentTime = dayjs().format("MM DD, YYYY")
         displayTime.text(currentTime);
       }
-    
+          
       toDisplayTime();
       setInterval(toDisplayTime, 1000);
       checkTense();
-
 
   });
